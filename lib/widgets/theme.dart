8ffe91acbad55c4757a4d7780a89e8444f526ba6
mehul nair <mehulnair32@gmail.com>
2022-10-15 23:12:55 +0530

    added themes and model

---
+++ lib/widgets/theme.dart
@@ -0,0 +1,21 @@
+import 'package:appdemo/custom_pallete.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: midBlue,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          titleTextStyle: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500)));
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
+      );
+}
