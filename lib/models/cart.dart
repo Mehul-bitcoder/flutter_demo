c9f53939bbc2496ec154236846592d93c71600a2
mehul nair <mehulnair32@gmail.com>
2022-10-24 18:15:27 +0530

    Cart model created

---
+++ lib/models/cart.dart
@@ -0,0 +1,31 @@
+import 'package:appdemo/models/catalog.dart';
+
+class CartModel {
+  //catalog field
+  late CatalogModel _catalog;
+  //Collections of id
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  //get items in cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //get total value
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // addItems
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -13,6 +13,65 @@ class Cart extends StatelessWidget {
         title: "Cart".text.make(),
       ),
       backgroundColor: context.canvasColor,
+      body: Column(children: [
+        const _CartList().p32().expand(),
+        const Divider(),
+        _CartTotal()
+      ]),
     );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999"
+              .text
+              .xl5
+              .color(Theme.of(context).colorScheme.secondary)
+              .make(),
+          30.widthBox,
+          ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: "Buying not supported yet".text.make()));
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(context.primaryColor)),
+                  child: "Buy".text.make())
+              .w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+              leading: const Icon(Icons.done),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.remove_circle_outline),
+              ),
+              title: "Item 1".text.make(),
+            ));
   }
 }
